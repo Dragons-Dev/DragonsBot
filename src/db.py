af7a17e0347ab13ee87101d71c09010cd105361f
paul <paulnicolai.hennings@gmail.com>
2023-06-29 22:14:18 +0200

    update v1 Introduction economy system Added nuke command

--- src/db.py
+++ src/db.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+import typing as t
 
 import aiosqlite
 import discord
@@ -14,8 +15,12 @@ async def create_db() -> None:
     async with aiosqlite.connect(DB_PATH) as db:
         async with db.cursor() as cursor:
             cursor: aiosqlite.Cursor = cursor
-            await cursor.execute("CREATE TABLE IF NOT EXISTS economy (cash REAL, user INTEGER, guild INTEGER)")
-            await cursor.execute("CREATE TABLE IF NOT EXISTS commands (command TEXT, enabled INTEGER, guild INTEGER)")
+            await cursor.execute(
+                "CREATE TABLE IF NOT EXISTS economy (pocket INTEGER, bank INTEGER, user INTEGER, guild INTEGER)"
+            )
+            await cursor.execute(
+                "CREATE TABLE IF NOT EXISTS commands (command TEXT, enabled INTEGER, guild INTEGER)"
+            )
 
 
 async def command_enabled(command: str, guild: int, enabled: bool = None, nuke: int = None):
@@ -42,28 +47,38 @@ async def command_enabled(command: str, guild: int, enabled: bool = None, nuke: 
             await db.commit()
 
 
-async def get_member_cash(user: int, guild: int):
+async def make_member_cash(user: int, guild: int):
     async with aiosqlite.connect(DB_PATH) as db:
         async with db.cursor() as cursor:
             cursor: aiosqlite.Cursor = cursor
-            response = await cursor.execute("SELECT cash FROM economy where user = ? AND guild = ?",
+            await cursor.execute(
+                    f"INSERT INTO economy (pocket, bank, user, guild) VALUES (?, ?, ?, ?)",
+                    (0, 10000, user, guild))
+            await db.commit()
+
+
+async def get_member_cash(target: t.Literal["pocket", "bank"], user: int, guild: int):
+    async with aiosqlite.connect(DB_PATH) as db:
+        async with db.cursor() as cursor:
+            cursor: aiosqlite.Cursor = cursor
+            response = await cursor.execute(f"SELECT {target} FROM economy where user = ? AND guild = ?",
                                             (user, guild))
             response = await response.fetchone()
-
             if response is None:
-                await cursor.execute(
-                    "INSERT INTO economy (cash, user, guild) VALUES (?, ?, ?)",
-                    (100, user, guild))
-                await db.commit()
-                return tuple([100])
-            await db.commit()
+                await make_member_cash(user=user, guild=guild)
+                response = await get_member_cash(
+                    target=target,
+                    user=user,
+                    guild=guild
+                )
+
             return tuple(response)
 
 
-async def set_cash(cash: float, user: int, guild: int):
+async def set_member_cash(target: t.Literal["pocket", "bank"], amount: int, user: int, guild: int):
     async with aiosqlite.connect(DB_PATH) as db:
         async with db.cursor() as cursor:
             cursor: aiosqlite.Cursor = cursor
-            await cursor.execute("UPDATE economy SET cash = ? WHERE user = ? AND guild = ?",
-                                 (round(cash, 2), user, guild))
+            await cursor.execute(f"UPDATE economy SET {target} = ? WHERE user = ? AND guild = ?",
+                                 (amount, user, guild))
             await db.commit()
